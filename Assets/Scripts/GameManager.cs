fae194e062a5823df020995b4eea23779739bff6
AlexAE12356890 <alexacacioerencia@gmail.com>
2026-02-25 12:51:10 +0100

    GoombaKillCounter
    Hecho

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int killCounter = 0;
     public int coinCounter = 0;
+    public Text goombaText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +22,7 @@ public class GameManager : MonoBehaviour
     public void  AddKill()
     {
         killCounter++;
+        goombaText.text = killCounter.ToString();
     }
 
     public void AddCoin()
